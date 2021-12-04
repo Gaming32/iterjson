90e76c1e17b691d64ec3f997870100afd43bfd2a
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-12-04 08:37:27 -0600

    Bump version

--- java/src/test/java/io/github/gaming32/iterjson/ReaderTest.java
+++ java/src/test/java/io/github/gaming32/iterjson/ReaderTest.java
@@ -3,14 +3,21 @@ package io.github.gaming32.iterjson;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import io.github.gaming32.iterjson.values.ArrayValue;
+import io.github.gaming32.iterjson.values.JsonValue;
+
 public class ReaderTest {
+    @SuppressWarnings("rawtypes")
     public static void main(String[] args) throws IOException {
         try (JsonReader reader = new JsonReader(
             new InputStreamReader(
                 ReaderTest.class.getResourceAsStream("/test.json")
             )
         )) {
-            System.out.println(reader.getRoot().read());
+            // System.out.println(reader.getRoot().read());
+            for (JsonValue<Object> val : (ArrayValue)(JsonValue)reader.getRoot()) {
+                System.out.println(val);
+            }
         }
     }
 }
